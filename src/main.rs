b7721c20f119cf123c7d859a4f7c4b829693bac0
Christian Alexander <kakkoiichris@gmail.com>
2023-09-03 23:06:50 -0400

    The player is now asked if they want to play again.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
+use std::io::{stdin, stdout, Write};
+
+use crate::game::Game;
+
 mod game;
 mod tile;
 mod turn;
 
-use crate::game::Game;
-
 fn main() {
-    let mut game = Game::new();
+    'game_loop:
+    loop {
+        let mut game = Game::new();
 
-    game.play();
+        game.play();
+
+        loop {
+            print!("Play again? ");
+
+            let _ = stdout().flush();
+
+            let mut input = String::new();
+
+            match stdin().read_line(&mut input) {
+                Ok(_) => {}
+
+                Err(_) => continue
+            }
+
+            match input.trim().to_lowercase().as_str() {
+                "yes" | "y" => continue 'game_loop,
+
+                _ => break 'game_loop,
+            }
+        }
+    }
 }
